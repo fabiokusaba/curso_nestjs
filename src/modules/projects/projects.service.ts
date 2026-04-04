00eaed9120290799cb9466235488749d56140234
Fabio Kusaba <fabinhokb44@gmail.com>
2026-04-04 09:53:59 -0300

    adicionar crud de projetos

--- src/modules/projects/projects.service.ts
+++ src/modules/projects/projects.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+  findAll() {}
+
+  findById(id: string) {}
+
+  create(data: any) {}
+
+  update(id: string, data: any) {}
+
+  remove(id: string) {}
+}
