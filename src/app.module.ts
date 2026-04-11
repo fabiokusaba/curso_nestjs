5e7e0458446f6af1037fe1b7cccd934cd5bf5fa2
Fabio Kusaba <fabinhokb44@gmail.com>
2026-04-11 20:58:21 -0300

    criar estrutura de arquivos do modulo de users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { ProjectsModule } from './modules/projects/projects.module';
-import { PrismaService } from './prisma.service';
-import { TasksModule } from './modules/tasks/tasks.module';
+import { ProjectsModule } from './modules/projects/projects.module'
+import { TasksModule } from './modules/tasks/tasks.module'
+import { UsersModule } from './modules/users/users.module'
+import { PrismaService } from './prisma.service'
 
 @Module({
-  imports: [ProjectsModule, TasksModule],
+  imports: [ProjectsModule, TasksModule, UsersModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
