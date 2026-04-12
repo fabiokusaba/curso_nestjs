4e2a4639df7f09982152b1ea387f663894b33c7f
Fabio Kusaba <fabinhokb44@gmail.com>
2026-04-11 21:25:31 -0300

    criar as classes de service e controller de users

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,55 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common'
+import { UsersService } from './users.service'
 
-@Controller('users')
-export class UsersController {}
+@Controller({
+  version: '1',
+  path: 'users',
+})
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get()
+  findAll() {
+    return this.userService.findAll()
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseUUIDPipe) id: string) {
+    const user = await this.userService.findById(id)
+
+    if (!user) {
+      throw new NotFoundException('User not found')
+    }
+
+    return user
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() data: any) {
+    return await this.userService.create(data)
+  }
+
+  @Put(':id')
+  async update(@Param('id', ParseUUIDPipe) id: string, @Body() data: any) {
+    return await this.userService.update(id, data)
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Param('id', ParseUUIDPipe) id: string) {
+    return await this.userService.remove(id)
+  }
+}
