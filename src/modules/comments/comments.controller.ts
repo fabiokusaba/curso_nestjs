5c84e5344d4a0babdf3a4081b38a120cfe46d4d6
Fabio Kusaba <fabinhokb44@gmail.com>
2026-04-25 21:19:36 -0300

    adicionar modulo de comentarios

---
+++ src/modules/comments/comments.controller.ts
@@ -0,0 +1,75 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+  UseInterceptors,
+} from '@nestjs/common'
+import { ApiCreatedResponse, ApiNoContentResponse, ApiOkResponse } from '@nestjs/swagger'
+import { ValidateResourcesIds } from '../common/decorators/validate-resources-ids.decorator'
+import { ValidateResourcesIdsInterceptor } from '../common/interceptors/validate-resources-ids.interceptor'
+import { CommentFullDTO, CommentListItemDTO, CommentRequestDTO } from './comments.dto'
+import { CommentsService } from './comments.service'
+
+@Controller({
+  version: '1',
+  path: 'projects/:projectId/tasks/:taskId/comments',
+})
+@UseInterceptors(ValidateResourcesIdsInterceptor)
+export class CommentsController {
+  constructor(private readonly commentService: CommentsService) {}
+
+  @Get()
+  @ValidateResourcesIds()
+  @ApiOkResponse({ type: [CommentListItemDTO], description: 'Get all comments by task' })
+  findAllByTask(@Param('taskId', ParseUUIDPipe) taskId: string) {
+    return this.commentService.findAllByTask(taskId)
+  }
+
+  @Get(':commentId')
+  @ValidateResourcesIds()
+  @ApiOkResponse({ type: CommentFullDTO, description: 'Get comment by Id' })
+  findOne(
+    @Param('taskId', ParseUUIDPipe) taskId: string,
+    @Param('commentId', ParseUUIDPipe) commentId: string,
+  ) {
+    return this.commentService.findById(taskId, commentId)
+  }
+
+  @Post()
+  @ValidateResourcesIds()
+  @ApiCreatedResponse({ type: CommentListItemDTO, description: 'Create a new comment' })
+  @HttpCode(HttpStatus.CREATED)
+  create(@Param('taskId', ParseUUIDPipe) taskId: string, @Body() data: CommentRequestDTO) {
+    return this.commentService.create(taskId, data)
+  }
+
+  @Put(':commentId')
+  @ValidateResourcesIds()
+  @ApiOkResponse({ type: CommentListItemDTO, description: 'Update a comment' })
+  @HttpCode(HttpStatus.OK)
+  update(
+    @Param('taskId', ParseUUIDPipe) taskId: string,
+    @Param('commentId', ParseUUIDPipe) commentId: string,
+    @Body() data: CommentRequestDTO,
+  ) {
+    return this.commentService.update(taskId, commentId, data)
+  }
+
+  @Delete(':commentId')
+  @ValidateResourcesIds()
+  @ApiNoContentResponse({ description: 'Delete a comment' })
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(
+    @Param('taskId', ParseUUIDPipe) taskId: string,
+    @Param('commentId', ParseUUIDPipe) commentId: string,
+  ) {
+    return this.commentService.remove(taskId, commentId)
+  }
+}
